b8d9510d4fb29b7b109831e29743e0524759d384
beylar <beylaruz@gmail.com>
2024-02-25 16:00:00 +0200

    Grammar check using different keywords

--- GrammarCheck.js
+++ GrammarCheck.js
@@ -1,7 +1,7 @@
-let story = 'Last weekend, I took literally the most beautifull bike ride of my life. The route is called "The 9W to Nyack" and it stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it literally took me an entire day. I stopped at Riverbank State Park to take some artsy photos. It was a short stop, though, because I had a freaking long way to go. After a quick photo op at the very popular Little Red Lighthouse I began my trek across the George Washington Bridge into New Jersey. The GW is a breathtaking 4,760 feet long! I was already very tired by the time I got to the other side. An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautifull park along the coast of the Hudson. Something that was very surprising to me was that near the end of the route you literally cross back into New York! At this point, you are very close to the end.';
+let story = 'Last weekend, I took literally the most beautifull bike ride of my life. The route is called "The 9W to Nyack" and it stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it literally took me an entire day. I stopped at Riverbank State Park to take some artsy photos. It was a short stop, though, because I had a freaking long way to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey. The GW is a breathtaking 4,760 feet long! I was already very tired by the time I got to the other side. An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautifull park along the coast of the Hudson. Something that was very surprising to me was that near the end of the route you literally cross back into New York! At this point, you are very close to the end.';
 
 let storyWords = story.split(' ');
-//
+
 console.log(storyWords);
 console.log(storyWords.join(''));
 
@@ -10,3 +10,34 @@ let unnecessaryWord = 'literally';
 let misspelledWord = 'beautifull';
 
 let badWord = 'freaking';
+
+let count = 0;
+storyWords.forEach((word) => {
+    count++;
+});
+
+console.log(count);
+
+//Filtering the words to not contain the unnecessaryWord
+storyWords = storyWords.filter((word) => word !== unnecessaryWord);
+
+//checking for misspelled words using ternary method
+storyWords = storyWords.map((word) => (word === misspelledWord ? 'beautiful' : word));
+
+//Finding the index of the bad word
+let badWordIndex = storyWords.findIndex((word) => word === badWord);
+console.log(badWordIndex);
+
+//replacing the badWordIndex with the word "really"
+storyWords[badWordIndex] = 'really';
+
+//check if every word is 10 characters or less
+let lengthCheck = storyWords.every((word) => word.length <= 10);
+console.log(lengthCheck);
+
+//Find the word in the story that is more than 10 characters and replace it
+let longWord = storyWords.find((word) => word.length > 10);
+story = story.replace(longWord, 'stunning');
+
+//Output of the story after the changes
+console.log(story);
